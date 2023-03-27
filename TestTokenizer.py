b0002252ea6e16599387f347d72deb08dfbd5092
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2023-03-27 10:39:50 +0330

    add Tokenizer class and test

---
+++ TestTokenizer.py
@@ -0,0 +1,142 @@
+import unittest
+from lexer import Tokenizer
+
+class TestTokenizer(unittest.TestCase):
+    def test_comment(self):
+        data = '''
+        # This is a comment
+        1 + 2
+        '''
+        tokenizer = Tokenizer()
+        token_list = tokenizer.tokenize(data)
+        self.assertEqual(token_list[0].type, 'NUMBER')
+        self.assertEqual(token_list[0].value, 1)
+        self.assertEqual(token_list[1].type, 'PLUS')
+        self.assertEqual(token_list[1].value, '+')
+        self.assertEqual(token_list[2].type, 'NUMBER')
+        self.assertEqual(token_list[2].value, 2)
+
+    def test_string(self):
+        data = '''
+        def int main() {
+            var str x = "hello world";
+        }
+        '''
+
+        tokenizer = Tokenizer()
+        token_list = tokenizer.tokenize(data)
+        self.assertEqual(token_list[0].type, 'DEF')
+        self.assertEqual(token_list[0].value, 'def')
+        self.assertEqual(token_list[1].type, 'INT')
+        self.assertEqual(token_list[1].value, 'int')
+        self.assertEqual(token_list[2].type, 'ID')
+        self.assertEqual(token_list[2].value, 'main')
+        self.assertEqual(token_list[3].type, 'LPAREN')
+        self.assertEqual(token_list[3].value, '(')
+        self.assertEqual(token_list[4].type, 'RPAREN')
+        self.assertEqual(token_list[4].value, ')')
+        self.assertEqual(token_list[5].type, 'LBRACE')
+        self.assertEqual(token_list[5].value, '{')
+        self.assertEqual(token_list[6].type, 'VAR')
+        self.assertEqual(token_list[6].value, 'var')
+        self.assertEqual(token_list[7].type, 'STR')
+        self.assertEqual(token_list[7].value, 'str')
+        self.assertEqual(token_list[8].type, 'ID')
+        self.assertEqual(token_list[8].value, 'x')
+        self.assertEqual(token_list[9].type, 'EQUALS')
+        self.assertEqual(token_list[9].value, '=')
+        self.assertEqual(token_list[10].type, 'STRING')
+        self.assertEqual(token_list[10].value, "hello world")
+        self.assertEqual(token_list[11].type, 'SEMI')
+        self.assertEqual(token_list[11].value, ';')
+        self.assertEqual(token_list[12].type, 'RBRACE')
+        self.assertEqual(token_list[12].value, '}')
+
+    def test_escape_double_quote(self):
+        data = '''
+        def int main() {
+            var str x = "hello\\" world";
+        }
+        '''
+
+        tokenizer = Tokenizer()
+        token_list = tokenizer.tokenize(data)
+        self.assertEqual(token_list[0].type, 'DEF')
+        self.assertEqual(token_list[0].value, 'def')
+        self.assertEqual(token_list[1].type, 'INT')
+        self.assertEqual(token_list[1].value, 'int')
+        self.assertEqual(token_list[2].type, 'ID')
+        self.assertEqual(token_list[2].value, 'main')
+        self.assertEqual(token_list[3].type, 'LPAREN')
+        self.assertEqual(token_list[3].value, '(')
+        self.assertEqual(token_list[4].type, 'RPAREN')
+        self.assertEqual(token_list[4].value, ')')
+        self.assertEqual(token_list[5].type, 'LBRACE')
+        self.assertEqual(token_list[5].value, '{')
+        self.assertEqual(token_list[6].type, 'VAR')
+        self.assertEqual(token_list[6].value, 'var')
+        self.assertEqual(token_list[7].type, 'STR')
+        self.assertEqual(token_list[7].value, 'str')
+        self.assertEqual(token_list[8].type, 'ID')
+        self.assertEqual(token_list[8].value, 'x')
+        self.assertEqual(token_list[9].type, 'EQUALS')
+        self.assertEqual(token_list[9].value, '=')
+        self.assertEqual(token_list[10].type, 'STRING')
+        self.assertEqual(token_list[10].value, "hello\" world")
+        self.assertEqual(token_list[11].type, 'SEMI')
+        self.assertEqual(token_list[11].value, ';')
+        self.assertEqual(token_list[12].type, 'RBRACE')
+        self.assertEqual(token_list[12].value, '}')
+    
+    def test_id(self):
+        data = '''
+        # This is a comment
+        x + 2
+        '''
+        tokenizer = Tokenizer()
+        token_list = tokenizer.tokenize(data)
+        self.assertEqual(token_list[0].type, 'ID')
+        self.assertEqual(token_list[0].value, 'x')
+        self.assertEqual(token_list[1].type, 'PLUS')
+        self.assertEqual(token_list[1].value, '+')
+        self.assertEqual(token_list[2].type, 'NUMBER')
+        self.assertEqual(token_list[2].value, 2)
+    
+    def test_reserved(self):
+        data = '''
+        def int main() {
+            var int x = 0;
+        }
+        '''
+
+        tokenizer = Tokenizer()
+        token_list = tokenizer.tokenize(data)
+        self.assertEqual(token_list[0].type, 'DEF')
+        self.assertEqual(token_list[0].value, 'def')
+        self.assertEqual(token_list[1].type, 'INT')
+        self.assertEqual(token_list[1].value, 'int')
+        self.assertEqual(token_list[2].type, 'ID')
+        self.assertEqual(token_list[2].value, 'main')
+        self.assertEqual(token_list[3].type, 'LPAREN')
+        self.assertEqual(token_list[3].value, '(')
+        self.assertEqual(token_list[4].type, 'RPAREN')
+        self.assertEqual(token_list[4].value, ')')
+        self.assertEqual(token_list[5].type, 'LBRACE')
+        self.assertEqual(token_list[5].value, '{')
+        self.assertEqual(token_list[6].type, 'VAR')
+        self.assertEqual(token_list[6].value, 'var')
+        self.assertEqual(token_list[7].type, 'INT')
+        self.assertEqual(token_list[7].value, 'int')
+        self.assertEqual(token_list[8].type, 'ID')
+        self.assertEqual(token_list[8].value, 'x')
+        self.assertEqual(token_list[9].type, 'EQUALS')
+        self.assertEqual(token_list[9].value, '=')
+        self.assertEqual(token_list[10].type, 'NUMBER')
+        self.assertEqual(token_list[10].value, 0)
+        self.assertEqual(token_list[11].type, 'SEMI')
+        self.assertEqual(token_list[11].value, ';')
+        self.assertEqual(token_list[12].type, 'RBRACE')
+        self.assertEqual(token_list[12].value, '}')
+    
+if __name__ == '__main__':
+    unittest.main()
--- lexer.py
+++ lexer.py
@@ -1,11 +1,182 @@
 from ply.lex import lex
-from ply.yacc import yacc
 import re
+
+
+class Tokenizer:
+    reserved = {
+        'if' : 'IF',
+        'else' : 'ELSE',
+        'def' : 'DEF',
+        'return' : 'RETURN',
+        'int' : 'INT',
+        'str' : 'STR',
+        'vector' : 'VECTOR',
+        'var' : 'VAR',
+        'void' : 'VOID',
+        'while' : 'WHILE',
+        'for' : 'FOR',
+        'break' : 'BREAK',
+        'continue' : 'CONTINUE',
+        'switch' : 'SWITCH',
+        'length' : 'LENGTH',
+        'case' : 'CASE',
+        'default' : 'DEFAULT',
+        'sizeof' : 'SIZEOF',
+        'struct' : 'STRUCT',
+        'static' : 'STATIC',
+        'auto' : 'AUTO',
+        'to' : 'TO',
+        'const' : 'CONST',
+        'class' : 'CLASS',
+        'import' : 'IMPORT',
+        'from' : 'FROM',
+        'as' : 'AS',
+        'pass' : 'PASS',
+        'global' : 'GLOBAL',
+        'nonlocal' : 'NONLOCAL',
+        'lambda' : 'LAMBDA',
+        'try' : 'TRY',
+        'except' : 'EXCEPT',
+        'raise' : 'RAISE',
+        'with' : 'WITH',
+        'del' : 'DEL',
+        'in' : 'IN',
+        'is' : 'IS',
+        'True' : 'TRUE',
+        'False' : 'FALSE',
+        'None' : 'NONE',
+        'print' : 'PRINT',
+        'input' : 'INPUT',
+        'range' : 'RANGE',
+        'len' : 'LEN',
+        'open' : 'OPEN',
+        'close' : 'CLOSE',
+        'read' : 'READ',
+        'write' : 'WRITE',
+        'type' : 'TYPE',
+    }
+
+    tokens = [
+        # Literals (identifier, integer constant, float constant, string constant, char const)
+        'ID', 'TYPEID', 'NUMBER', 'STRING', 'CHARACTER',
+
+        # Operators (+,-,*,/,%,|,&,~,^,<<,>>, ||, &&, !, <, <=, >, >=, ==, !=)
+        'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MODULO',
+        'OR', 'AND', 'NOT', 'XOR', 'LSHIFT', 'RSHIFT',
+        'LOR', 'LAND', 'LNOT',
+        'LT', 'LE', 'GT', 'GE', 'EQ', 'NE',
+
+        # Assignment (=, *=, /=, %=, +=, -=, 
+        'EQUALS', 'TIMESEQUAL', 'DIVEQUAL', 'MODEQUAL', 'PLUSEQUAL', 'MINUSEQUAL',
+
+        # Increment/decrement (++,--)
+        'INCREMENT', 'DECREMENT',
+
+        # Delimeters ( ) [ ] { } , . ; :
+        'LPAREN', 'RPAREN',
+        'LBRACKET', 'RBRACKET',
+        'LBRACE', 'RBRACE',
+        'COMMA', 'PERIOD', 'SEMI', 'COLON',
+
+    ] + list(reserved.values())
+
+    t_ignore = ' \t'
+    # Operators
+    t_PLUS             = r'\+'
+    t_MINUS            = r'-'
+    t_TIMES            = r'\*'
+    t_DIVIDE           = r'/'
+    t_MODULO           = r'%'
+    t_OR               = r'\|'
+    t_AND              = r'&'
+    t_NOT              = r'~'
+    t_XOR              = r'\^'
+    t_LSHIFT           = r'<<'
+    t_RSHIFT           = r'>>'
+    t_LOR              = r'\|\|'
+    t_LAND             = r'&&'
+    t_LNOT             = r'!'
+    t_LT               = r'<'
+    t_GT               = r'>'
+    t_LE               = r'<='
+    t_GE               = r'>='
+    t_EQ               = r'=='
+    t_NE               = r'!='
+
+    # Assignment operators
+
+    t_EQUALS           = r'='
+    t_TIMESEQUAL       = r'\*='
+    t_DIVEQUAL         = r'/='
+    t_MODEQUAL         = r'%='
+    t_PLUSEQUAL        = r'\+='
+    t_MINUSEQUAL       = r'-='
+
+    # Increment/decrement
+    t_INCREMENT        = r'\+\+'
+    t_DECREMENT        = r'--'
+
+
+    # Delimeters
+    t_LPAREN           = r'\('
+    t_RPAREN           = r'\)'
+    t_LBRACKET         = r'\['
+    t_RBRACKET         = r'\]'
+    t_LBRACE           = r'\{'
+    t_RBRACE           = r'\}'
+    t_COMMA            = r','
+    t_PERIOD           = r'\.'
+    t_SEMI             = r';'
+    t_COLON            = r':'
+
+
+    def __init__(self) -> None:
+        self.lexer = lex(module=self)
+
+    def tokenize(self, data):
+        token_list = []
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            token_list.append(tok)
+            print(tok)
+        return token_list
+
+    def t_NUMBER(self, t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
+    
+    def t_STRING(self, t):
+        # String literal - (x|y) where x shouldn't match \ , \n , "
+        r'\"([^\\\n]|(\\.))*?\"'
+        t.value = re.sub(r'\\', r'', t.value[1:-1])
+        return t
+
+    def t_ID(self, t):
+        r'[A-Za-z_][A-Za-z0-9_]*'
+        t.type = self.reserved.get(t.value, 'ID')    # Check for reserved words. If not reserved, it is an ID
+        return t
+
+    def t_COMMENT(self, t):
+        r'\#.*'
+        pass
+
+    # Ignored token with an action associated with it
+    def t_ignore_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += t.value.count('\n')
+
+    # Error handler for illegal characters
+    def t_error(self, t):
+        print(f'Illegal character {t.value[0]!r}')
+        t.lexer.skip(1)
 
 
 
 data = """
-
     def int main() {
         var str x = "hello";
     }
@@ -52,181 +223,11 @@ data = """
     # }
 
 """
-
-print(data)
-
-
+tokenizer = Tokenizer()
+token_list = tokenizer.tokenize(data)
 
 
 
-
-
-# --- Tokenizer
-
-reserved = {
-    'if' : 'IF',
-    'else' : 'ELSE',
-    'def' : 'DEF',
-    'return' : 'RETURN',
-    'int' : 'INT',
-    'str' : 'STR',
-    'vector' : 'VECTOR',
-    'var' : 'VAR',
-    'void' : 'VOID',
-    'while' : 'WHILE',
-    'for' : 'FOR',
-    'break' : 'BREAK',
-    'continue' : 'CONTINUE',
-    'switch' : 'SWITCH',
-    'length' : 'LENGTH',
-    'case' : 'CASE',
-    'default' : 'DEFAULT',
-    'sizeof' : 'SIZEOF',
-    'struct' : 'STRUCT',
-    'static' : 'STATIC',
-    'auto' : 'AUTO',
-    'to' : 'TO',
-    'const' : 'CONST',
-    'class' : 'CLASS',
-    'import' : 'IMPORT',
-    'from' : 'FROM',
-    'as' : 'AS',
-    'pass' : 'PASS',
-    'global' : 'GLOBAL',
-    'nonlocal' : 'NONLOCAL',
-    'lambda' : 'LAMBDA',
-    'try' : 'TRY',
-    'except' : 'EXCEPT',
-    'raise' : 'RAISE',
-    'with' : 'WITH',
-    'del' : 'DEL',
-    'in' : 'IN',
-    'is' : 'IS',
-    'True' : 'TRUE',
-    'False' : 'FALSE',
-    'None' : 'NONE',
-    'print' : 'PRINT',
-    'input' : 'INPUT',
-    'range' : 'RANGE',
-    'len' : 'LEN',
-    'open' : 'OPEN',
-    'close' : 'CLOSE',
-    'read' : 'READ',
-    'write' : 'WRITE',
-    'type' : 'TYPE',
-}
-
-tokens = [
-    # Literals (identifier, integer constant, float constant, string constant, char const)
-    'ID', 'TYPEID', 'NUMBER', 'STRING', 'CHARACTER',
-
-    # Operators (+,-,*,/,%,|,&,~,^,<<,>>, ||, &&, !, <, <=, >, >=, ==, !=)
-    'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MODULO',
-    'OR', 'AND', 'NOT', 'XOR', 'LSHIFT', 'RSHIFT',
-    'LOR', 'LAND', 'LNOT',
-    'LT', 'LE', 'GT', 'GE', 'EQ', 'NE',
-
-    # Assignment (=, *=, /=, %=, +=, -=, 
-    'EQUALS', 'TIMESEQUAL', 'DIVEQUAL', 'MODEQUAL', 'PLUSEQUAL', 'MINUSEQUAL',
-
-    # Increment/decrement (++,--)
-    'INCREMENT', 'DECREMENT',
-
-    # Delimeters ( ) [ ] { } , . ; :
-    'LPAREN', 'RPAREN',
-    'LBRACKET', 'RBRACKET',
-    'LBRACE', 'RBRACE',
-    'COMMA', 'PERIOD', 'SEMI', 'COLON',
-
-] + list(reserved.values())
-
-
-t_ignore = ' \t'
-# Operators
-t_PLUS             = r'\+'
-t_MINUS            = r'-'
-t_TIMES            = r'\*'
-t_DIVIDE           = r'/'
-t_MODULO           = r'%'
-t_OR               = r'\|'
-t_AND              = r'&'
-t_NOT              = r'~'
-t_XOR              = r'\^'
-t_LSHIFT           = r'<<'
-t_RSHIFT           = r'>>'
-t_LOR              = r'\|\|'
-t_LAND             = r'&&'
-t_LNOT             = r'!'
-t_LT               = r'<'
-t_GT               = r'>'
-t_LE               = r'<='
-t_GE               = r'>='
-t_EQ               = r'=='
-t_NE               = r'!='
-
-# Assignment operators
-
-t_EQUALS           = r'='
-t_TIMESEQUAL       = r'\*='
-t_DIVEQUAL         = r'/='
-t_MODEQUAL         = r'%='
-t_PLUSEQUAL        = r'\+='
-t_MINUSEQUAL       = r'-='
-
-# Increment/decrement
-t_INCREMENT        = r'\+\+'
-t_DECREMENT        = r'--'
-
-
-# Delimeters
-t_LPAREN           = r'\('
-t_RPAREN           = r'\)'
-t_LBRACKET         = r'\['
-t_RBRACKET         = r'\]'
-t_LBRACE           = r'\{'
-t_RBRACE           = r'\}'
-t_COMMA            = r','
-t_PERIOD           = r'\.'
-t_SEMI             = r';'
-t_COLON            = r':'
-
-# String literal - (x|y) where x shouldn't match \ , \n , "
-t_STRING = r'\"([^\\\n]|(\\.))*?\"'
-
-
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-def t_ID(t):
-    r'[A-Za-z_][A-Za-z0-9_]*'
-    t.type = reserved.get(t.value, 'ID')    # Check for reserved words. If not reserved, it is an ID
-    return t
-
-def t_COMMENT(t):
-    r'\#.*'
-    pass
-
-# Ignored token with an action associated with it
-def t_ignore_newline(t):
-    r'\n+'
-    t.lexer.lineno += t.value.count('\n')
-
-# Error handler for illegal characters
-def t_error(t):
-    print(f'Illegal character {t.value[0]!r}')
-    t.lexer.skip(1)
-
-# Build the lexer object
-lexer = lex()
-
-
-
-lexer.input(data)
-
-for tok in lexer:
-    print(tok)
 
 
 
